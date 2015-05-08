60431e060f589499149f7a41b48d0afb496d1754
DianouPac <dmo.pacaud@gmail.com>
2015-05-08 21:43:48 +0200

    apport 03

--- Projet_LO07/security/auth_user.php
+++ Projet_LO07/security/auth_user.php
@@ -1,5 +1,22 @@
 <?php
-require_once __DIR__.'../mysql_connect.php';
-if(isset($_POST['login']) && isset($_POST['mdp'])) {
-    /* verif bdd*/
+require_once __DIR__.'/../mysql_connect.php';
+session_start();
+if(!(isset($_SESSION['auth'])) /*|| $_SESSION['auth'] == "" */|| (!($_SESSION['auth']))) {
+    $auth = FALSE;
+    echo 'toto';
+    if(isset($_POST['login']) && isset($_POST['mdp'])) {
+
+        $query = mysqli_query($bdd, 'SELECT COUNT(*) FROM individu WHERE login="'.$_POST['login'].'" AND pass="'.$_POST['mdp'].'"');
+        $result = mysqli_fetch_array($query,MYSQLI_NUM);
+        $auth = $result[0] > 0;
+        
+    }
+    
+    if($auth) {
+       $_SESSION['auth'] = TRUE;
+    }else {
+        //Erreur authentification
+        $_SESSION['flashbag'] = 'Mauvais nom d\'utilisateur ou mauvais mot de passe';
+        header('Location:'.__DIR__.'/../inscription_connexion/form_connexion.php');
+    }
 }
