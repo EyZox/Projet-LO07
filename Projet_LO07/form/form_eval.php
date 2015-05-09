887e5e73504ac77df9b53a987381a898be5156ea
DianouPac <dmo.pacaud@gmail.com>
2015-05-09 16:57:27 +0200

    apport 04

---
+++ Projet_LO07/form/form_eval.php
@@ -0,0 +1,12 @@
+<form method="Post" action="">
+    <select name="note">
+        <option value="1">A éviter</option>
+        <option value="2">Décevant</option>
+        <option value="3">Bien</option>
+        <option value="4">Excellent</option>
+        <option value="5">Extraordinaire</option>
+    </select>
+    <p>
+        <textarea>Laissez un commentaire</textarea>
+    </p>
+</form>
