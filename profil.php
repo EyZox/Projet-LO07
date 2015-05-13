0a23fba00e3bfc3bba538acaec6c8b63e9dff0bb
Alexandre DUPONCHEL <alexandre.duponchel@gmail.com>
2015-05-13 21:58:04 +0200

    correction titre page profil

--- profil.php
+++ profil.php
@@ -2,5 +2,5 @@
 require_once __DIR__.'/global.php';
 require_once ROOT.'security/auth_user.php';
 
-build_template('profil-global.php', array('title' => 'Votre tableau de bord'));
+build_template('profil-global.php', array('title' => 'Votre profil'));
 ?>
